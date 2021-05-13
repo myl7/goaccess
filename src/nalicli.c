58235169b6ba1cd54ce50b9cb145ae35a432b8d3
myl7 <myl@myl.moe>
2021-05-14 05:25:03 +0800

    Add license info

--- src/nalicli.c
+++ src/nalicli.c
@@ -8,6 +8,7 @@
  *
  * The MIT License (MIT)
  * Copyright (c) 2009-2020 Gerardo Orellana <hello @ goaccess.io>
+ * Modifications Copyright (c) 2021 myl7 <myl @ myl.moe>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
